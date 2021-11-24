39d1570d1fcbd97b42f05bc8ec411ea4f58e5c98
Reptylia <repty.alliance@gmail.com>
2021-11-24 19:56:12 +0300

    refactor: 3.4 rules change-tag-name

--- app.js
+++ app.js
@@ -33,20 +33,25 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='label task';
+   
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="input";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="input task";
+   
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="button edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="img";
     deleteButton.appendChild(deleteButtonImg);
-
+    
+    listItem.className="li";
 
     //and appending.
     listItem.appendChild(checkBox);
